d2ed5dc53c40aef17f15c520485c5e9c4e62fa06
ahmed-elshamy23 <ahmedm.elshamy23@gmail.com>
2025-04-25 13:05:37 +0200

    Added Movie Card

--- Front-End/movies-streaming-app/src/app/components/shared/movie-card/movie-card.component.ts
+++ Front-End/movies-streaming-app/src/app/components/shared/movie-card/movie-card.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { IconComponent } from '../icon-component/icon.component';
 
 @Component({
   selector: 'app-movie-card',
-  imports: [],
+  imports: [IconComponent],
   templateUrl: './movie-card.component.html',
   styleUrl: './movie-card.component.css'
 })
 export class MovieCardComponent {
-
+  name: string = "Ratatouille";
+  hours: number = 1;
+  minutes: number = 45;
+  posterUrl: string = "https://belovedpawn.org/wp-content/uploads/2024/04/ratatouille-in-hermann-mo.jpeg";
+  cast: string[] = ["aaaa", "aaaa"];
 }
