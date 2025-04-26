92eaf77723b0feb5fa5dc8d37a0f1b4a89203645
miinamaaher1 <minamaher1515@gmail.com>
2025-04-26 17:53:46 +0300

    removed iframe fom hero

--- Front-End/movies-streaming-app/src/app/components/hero-banner/hero-banner.component.ts
+++ Front-End/movies-streaming-app/src/app/components/hero-banner/hero-banner.component.ts
@@ -13,32 +13,27 @@ export interface movieBanner {
   templateUrl: './hero-banner.component.html'
 })
 export class HeroBannerComponent implements OnChanges, OnInit {
-  constructor(private sanitizer: DomSanitizer) {}
+  constructor(/*private sanitizer: DomSanitizer*/) {}
 
   banners : movieBanner[] = [
     {
       poster: "images/posters/harry.jpg",
-      // clip: "VyHV0BRtdxo"
       clip: "videos/clips/H_P_Clip.mp4"
     },
     {
       poster: "images/posters/breaking.jpg",
-      // clip: "VFkjBy2b50Q"
       clip: "videos/clips/B_B_Clip.mp4"
     },
     {
       poster: "images/posters/star.jpg",
-      // clip: "Q0CbN8sfihY"
       clip: "videos/clips/S_W_Clip.mp4"
     },
     {
       poster: "images/posters/grinch.jpg",
-      // clip: "TdSdIjb8N9M"
       clip: "videos/clips/H_G_S_C_Clip.mp4"
     },
     {
       poster: "images/posters/thrones.jpg",
-      // clip: "KPLWWIOCOOQ"
       clip: "videos/clips/G_O_T_Clip.mp4"
     }
   ]
@@ -66,10 +61,10 @@ export class HeroBannerComponent implements OnChanges, OnInit {
     this.isMuted = !this.isMuted;
   }
 
-  getSafeUrl(key: string): SafeResourceUrl {
-    const url = `https://www.youtube.com/embed/${key}?autoplay=1&mute=1&controls=0&loop=1&playlist=${key}&modestbranding=1&rel=0&showinfo=0`;
-    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
-  }
+  // getSafeUrl(key: string): SafeResourceUrl {
+  //   const url = `https://www.youtube.com/embed/${key}?autoplay=1&mute=1&controls=0&loop=1&playlist=${key}&modestbranding=1&rel=0&showinfo=0`;
+  //   return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  // }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['index'] && !changes['index'].firstChange) {
