9ba1dbc420192588fa6fd5516641e93d627941e7
mohamed khaled <first99ali@gmail.com>
2025-04-30 19:33:21 +0300

    done signin

--- Front-End/movies-streaming-app/src/app/app.config.ts
+++ Front-End/movies-streaming-app/src/app/app.config.ts
@@ -5,11 +5,27 @@ import { HttpClientModule } from '@angular/common/http'; // Import HttpClientMod
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    importProvidersFrom(HttpClientModule), // Import HttpClientModule providers
+    importProvidersFrom(HttpClientModule), // Import HttpClientModule providers'
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+          preset: Aura,
+          options: {
+              cssLayer: {
+                  name: 'primeng',
+                  order: 'theme, base, primeng'
+              }
+          }
+      }
+  })
   ],
 };
