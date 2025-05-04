fd0613253ab2b5d5b027ff9cc93558c502d013f3
ahmed-elshamy23 <ahmedm.elshamy23@gmail.com>
2025-05-04 12:30:11 +0300

    Added Subscription Controller

--- Back-End/Movie_Streaming_App/Movie_Streaming_App/Controllers/SubscriptionController.cs
+++ Back-End/Movie_Streaming_App/Movie_Streaming_App/Controllers/SubscriptionController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Movie_Streaming_App.Controllers
 {
+    [Authorize]
     [Route("api/subscriptions")]
     [ApiController]
     public class SubscriptionController : ControllerBase
