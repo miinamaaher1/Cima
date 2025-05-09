4321f63806061e8fb716e0df22989df5e6df6555
abdelrahmanramadan12 <abdelrahmanramadan416@gmail.com>
2025-05-10 00:13:10 +0300

    Bind favorite and watchlist data & Added toogle list function in movie and series card

--- Front-End/movies-streaming-app/src/app/components/shared/episode-card/episode-card.component.ts
+++ Front-End/movies-streaming-app/src/app/components/shared/episode-card/episode-card.component.ts
@@ -1,17 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, input, Input, OnInit } from '@angular/core';
 import { IconComponent } from "../icon-component/icon.component";
 import { CommonModule } from '@angular/common';
 import { FormatTimePipe } from '../../../core/pipes/format-time.pipe';
+import { RouterLink } from '@angular/router';
+import { AccountService } from '../../../core/services/Account/account.service';
 
 @Component({
   selector: 'app-episode-card',
-  imports: [CommonModule,IconComponent,FormatTimePipe],
+  imports: [CommonModule,FormatTimePipe,RouterLink],
   templateUrl: './episode-card.component.html'
 })
-export class EpisodeCardComponent {
+export class EpisodeCardComponent implements OnInit{
+  constructor(private accountService:AccountService){}
+  ngOnInit(): void {
+    this.isAuthenticated= this.accountService.isLoggedIn()
+  }
+  seasonNumber=input.required<number>();
+  seriesId=input.required<number>();
   @Input() episodeId:number=0;
   @Input() Poster:string="/images/posters/harry.jpg"
   @Input() episodeNum:number=0
   @Input() Runtime:number=0;
   @Input() Description:string='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi laborum vel soluta aspernatur blanditiis provident odio nesciunt? Tenetur, iusto ex.';
+
+  isAuthenticated:boolean=false;
+
+
+  
+
 }
--- Front-End/movies-streaming-app/src/app/pages/my-lists/my-lists.component.ts
+++ Front-End/movies-streaming-app/src/app/pages/my-lists/my-lists.component.ts
@@ -1,15 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MovieCardComponent } from "../../components/shared/movie-card/movie-card.component";
 import { SerieCardComponent } from "../../components/shared/serie-card/serie-card.component";
+import { FavoritesService } from '../../core/services/Favoutites/favourites.service';
+import { VideoType } from '../../core/dtos/VideoType';
+import { WatchlistService } from '../../core/services/Watchlist/watchlist.service';
 
 @Component({
   selector: 'app-my-lists',
   imports: [MovieCardComponent, SerieCardComponent],
   templateUrl: './my-lists.component.html'
 })
-export class MyListsComponent {
-  watchlistSeriesIds : number[] = [13945, 13945, 13945, 13945, 13945, 13945, 13945]
-  watchlistMovieIds : number[] = [1124620, 1124620, 1124620, 1124620, 1124620, 1124620]
-  favoriteSeriesIds : number[] = [13945, 13945, 13945, 13945, 13945, 13945, 13945]
-  favoriteMovieIds : number[] = [1124620, 1124620, 1124620, 1124620, 1124620, 1124620]
+export class MyListsComponent implements OnInit {
+
+  isLoading: boolean = false;
+  watchlistSeriesIds: number[] = [13945, 13945, 13945, 13945, 13945, 13945, 13945]
+  watchlistMovieIds: number[] = [1124620, 1124620, 1124620, 1124620, 1124620, 1124620]
+  favoriteSeriesIds: number[] = [13945, 13945, 13945, 13945, 13945, 13945, 13945]
+  favoriteMovieIds: number[] = [1124620, 1124620, 1124620, 1124620, 1124620, 1124620]
+
+  constructor(private _favoritesService: FavoritesService, private _watchlistService: WatchlistService) { }
+
+  ngOnInit(): void {
+    this.fetchFavoriteMedia();
+    this.fetchWatchlistMedia();
+  }
+
+  fetchFavoriteMedia() {
+    this.isLoading = true;
+    let favMovies: number[] = []
+    let favSeries: number[] = []
+
+    this._favoritesService.getFavorites().subscribe({
+      next: fav => {
+        fav.forEach(fav => {
+          if (fav.videoType === VideoType.Movie) {
+            favMovies.push(fav.id);
+          }
+          else {
+            favSeries.push(fav.id);
+          }
+        })
+        this.favoriteMovieIds = favMovies;
+        this.favoriteSeriesIds = favSeries;
+        this.isLoading = false;
+      },
+      error: err => {
+        this.isLoading = false;
+        console.error(err);
+      }
+    })
+  }
+
+  fetchWatchlistMedia() {
+    this.isLoading = true;
+    let watchMovies: number[] = []
+    let watchSeries: number[] = []
+
+    this._watchlistService.getWatchlist().subscribe({
+      next: wlist => {
+        wlist.forEach(w => {
+          if (w.videoType === VideoType.Movie) {
+            watchMovies.push(w.id);
+          }
+          else {
+            watchSeries.push(w.id);
+          }
+        })
+        this.watchlistMovieIds = watchMovies;
+        this.watchlistSeriesIds = watchSeries;
+        this.isLoading = false;
+      },
+      error: err => {
+        this.isLoading = false;
+        console.error(err);
+      }
+    })
+  }
+
+
 }
