bf5e093021bfe3271865aaca1fac559252e4ff1e
abdelrahmanramadan12 <abdelrahmanramadan416@gmail.com>
2025-05-10 18:16:20 +0300

    Fixed small Nav

--- Front-End/movies-streaming-app/src/app/components/nav/nav-sm/nav-sm.component.ts
+++ Front-End/movies-streaming-app/src/app/components/nav/nav-sm/nav-sm.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { AccountService } from '../../../core/services/Account/account.service';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../../core/services/user/user.service';
+
+
 
 @Component({
   selector: 'app-nav-sm',
@@ -16,7 +18,10 @@ export class NavSmComponent {
     public _userService : UserService
   ) {}
 
-
+  @Output() Mina=new EventEmitter()
+  Close(){
+    this.Mina.emit()
+  }
 
   logout() {
     this._accountService.logout()
